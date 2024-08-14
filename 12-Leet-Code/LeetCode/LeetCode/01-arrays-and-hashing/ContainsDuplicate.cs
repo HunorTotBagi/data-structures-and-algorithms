f3f65f91c0fd5493f81e3fc7a6ce8262f81afb59
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-08-14 11:03:17 +0200

    add contains duplicate in C#

---
+++ 12-Leet-Code/LeetCode/LeetCode/01-arrays-and-hashing/ContainsDuplicate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public static class ContainsDuplicate
+    {
+        public static bool CheckForDuplicates(int[] array)
+        {
+            var seenNumbers = new HashSet<int>(array.Length);
+
+            foreach (int element in array)
+            {
+                if (seenNumbers.Contains(element))
+                {
+                    return true;
+                }
+                seenNumbers.Add(element);
+            }
+            return false;
+        }
+    }
+}
