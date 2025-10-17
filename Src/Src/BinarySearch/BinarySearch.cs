7c68a2e527e6c301da9018ff7cbd73202b05a508
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-17 08:46:50 +0200

    feat: converting into C#

---
+++ Src/Src/BinarySearch/BinarySearch.cs
@@ -0,0 +1,24 @@
+﻿namespace Src.BinarySearch;
+
+public class BinarySearch
+{
+    public static int BasicBinarySearch(int[] array, int target)
+    {
+        var start = 0;
+        var end = array.Length;
+
+        while (start < end)
+        {
+            int middle = start + (end - start) / 2;
+
+            if (target < array[middle])
+                end = middle - 1;
+            else if (target > array[middle])
+                start = middle + 1;
+            else
+                return middle;
+        }
+
+        return -1;
+    }
+}
---
+++ Tests/Tests/BinarySearch/BinarySearchTests.cs
@@ -0,0 +1,22 @@
+﻿using static Src.BinarySearch.BinarySearch;
+using FluentAssertions;
+
+namespace Tests.BinarySearch;
+
+public class BinarySearchTests
+{
+    [Fact]
+    public void SimpleTest()
+    {
+        // Arrange
+        var array = new[] { 2, 4, 6, 9, 11, 12, 14, 20, 36, 48 };
+        var target = 36;
+        var targetIndex = 8;
+
+        // Act
+        var result = BasicBinarySearch(array, target);
+
+        // Assert
+        result.Should().Be(targetIndex);
+    }
+}
