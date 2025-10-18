0ea9e87784da7f92f543f84a7597a716e92741af
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-18 07:04:00 +0200

    chore: remove redundant parentheses

--- Src/SearchingAlgorithms/ClassicLinearSearch.cs
+++ Src/SearchingAlgorithms/ClassicLinearSearch.cs
@@ -5,16 +5,12 @@ public class ClassicLinearSearch
     public static int LinearSearch(int[] array, int target)
     {
         if (array.Length == 0)
-        {
             return -1;
-        }
 
         for (var i = 0; i < array.Length; i++)
         {
             if (array[i] == target)
-            {
                 return i;
-            }
         }
 
         return -1;
